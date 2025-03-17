b85bc962f5fd5228e7755582bc11d4c5c7f9f2dd
k-redstone <che0618@naver.com>
2025-03-18 00:10:35 +0900

    chore: footer 정보 수정

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,10 +1,11 @@
 function Footer() {
   return (
     <footer className="border-border/40 border-t py-6 md:py-0">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-5 md:h-16 md:flex-row">
         <p className="text-muted-foreground text-sm">
           오류 제보 / 기능 개선 문의 : che06189@gmail.com
         </p>
+        <p className="text-muted-foreground text-sm">게임 기록 제공 : AK</p>
       </div>
     </footer>
   )
