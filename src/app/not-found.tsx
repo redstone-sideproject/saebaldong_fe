bebbadd709ca3a89ff9f5173004970c57ee97d18
k-redstone <che0618@naver.com>
2025-03-17 19:42:33 +0900

    feat: not-found 페이지 제작

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col">
+      <main className="mx-auto flex-1">
+        <section className="container py-24">
+          <div className="mx-auto flex max-w-[980px] flex-col items-center gap-4 text-center">
+            <p>해당 페이지를 찾을 수 없습니다.</p>
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
