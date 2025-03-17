6500d37fc1e3eb99de1983cfd5503ba2dc8c159c
k-redstone <che0618@naver.com>
2025-03-17 15:26:33 +0900

    feat: 참여도 정렬 기준 추가

--- src/api/streamer/index.ts
+++ src/api/streamer/index.ts
@@ -11,7 +11,21 @@ async function fetchStreamersWithStatus(params: {
   sortField?: SortFieldUnion
   sortOrder?: SortOrderUnion
 }): Promise<IStreamerWithStatus[]> {
+  if (params.sortField === 'participationRatio') {
+    const result = await publicAPI.get(`/streamer`)
+    const sortByParticipationRatio: IStreamerWithStatus[] = result.data
+
+    return params.sortOrder === 'DESC'
+      ? sortByParticipationRatio.toSorted(
+          (a, b) => b.participationRatio - a.participationRatio,
+        )
+      : sortByParticipationRatio.toSorted(
+          (a, b) => a.participationRatio - b.participationRatio,
+        )
+  }
+
   const result = await publicAPI.get(`/streamer`, { params })
+
   return result.data
 }
 
